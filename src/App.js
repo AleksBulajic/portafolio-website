f51dd3794d6a48cd5c9fb5733e7f0601ec7d398a
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-07-25 13:43:01 -0400

    animation for the portafolio

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import ParticlesComponent from "./components/Particles";
 
 function App() {
   return (
     <>
+    <div className="particles-container">
+    <ParticlesComponent />
+    </div>
       <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Particles.js
@@ -0,0 +1,62 @@
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim"; // loads tsparticles-slim
+// import { loadFull } from "tsparticles"; // loads tsparticles
+import { useCallback, useMemo } from "react";
+
+const ParticlesComponent = (props) => {
+  const options = useMemo(() => {
+    return {
+      background: {
+        color: "#000",
+      },
+      fullScreen: {
+        enable: true,
+      },
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 10,
+          },
+          repulse: {
+            distance: 100,
+          },
+        },
+      },
+      particles: {
+        links: {
+          enable: true,
+          distance: 200,
+        },
+        move: {
+          enable: true,
+          speed: { min: 1, max: 5 },
+        },
+        opacity: {
+          value: { min: 0.3, max: 0.7 },
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    };
+  }, []);
+
+  const particlesInit = useCallback((engine) => {
+    loadSlim(engine);
+  }, []);
+
+  return <Particles id={props.id} init={particlesInit} options={options} />;
+};
+
+export default ParticlesComponent;
