f0dd212914e0514b4668780c2b82bc8b17af68ce
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-07-25 16:37:54 -0400

    aniation working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
